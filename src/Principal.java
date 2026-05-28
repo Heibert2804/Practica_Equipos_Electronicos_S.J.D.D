c6a0069b2cd961e0543cf6f47415be268436d655
Sala M_101 <salam101_2@salasfr.itm.edu.co>
2026-05-28 10:57:17 -0500

    HU1 estructura inicial del proyecto

--- src/Principal.java
+++
@@ -1,8 +0,0 @@
-package src;
-public class Principal {
-    public static void main(String[] args){
-        System.out.println("Sistema de prestamos iniciado");
-        Menus m = new Menus();
-        m.menupricipal();
-    }
-}
