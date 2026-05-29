95b08b0a964b70fb2a0dcd03d1b98b20394acb80
Heibert2804 <heibertaguaidoa@gmail.com>
2026-05-29 12:18:23 -0500

    Update Main.java

---
+++ src/principal/Main.java
@@ -0,0 +1,14 @@
+package principal;
+
+import menus.Menu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando SISTEMA de prestamos");
+        Menu menu = new Menu();
+        menu.menupricipal();
+
+    }
+
+}
